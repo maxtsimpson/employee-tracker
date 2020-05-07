40783781529e368f7d54a7b0aaeb50f0b45e9867
Max Simpson <maxsimpson95@gmail.com>
2020-05-08 06:18:22 +0800

    initial SQL to create db and tables. started folder layout

---
+++ Develop/db/scripts/createSchema.sql
@@ -0,0 +1,27 @@
+DROP DATABASE IF EXISTS employeeTracker;
+CREATE DATABASE employeeTracker;
+USE employeeTracker;
+CREATE TABLE department (
+  id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(30) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE `role` (
+  id INT NOT NULL AUTO_INCREMENT,
+  title VARCHAR(30) NULL,
+  salary DECIMAL(10,2) NULL,
+  department_id INT,
+  FOREIGN KEY (department_id) references department(id),
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE employee (
+  id INT NOT NULL AUTO_INCREMENT,
+  first_name VARCHAR(30) NULL,
+  last_name VARCHAR(30) NULL,
+  role_id INT,
+  FOREIGN KEY (role_id) references role(id),
+  manager_id INT NULL,
+  PRIMARY KEY (id)
+);

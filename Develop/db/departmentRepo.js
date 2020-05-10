48e5c723f132f73f99d2f282815fe6f77fcf16b3
Max Simpson <maxsimpson95@gmail.com>
2020-05-10 20:23:11 +0800

    fleshed out department and role repo

--- Develop/db/departmentRepo.js
+++ Develop/db/departmentRepo.js
@@ -1,31 +1,68 @@
-var mysql = require('mysql');
-var Deparment = require("../model/deparment")
+var Deparment = require("../model/department")
 
 class departmnetRepositry {
 
-    constructor(mySQLConnection) {
-        this.connection = mySQLConnection;
-
-        this.createQuery = ``
-        this.selectAllQuery = ``
-        this.updateQuery = ``
-        this.deleteQuery = ``
+    constructor(db) {
+        this.db = db; //this is an instance of mySQLDB 
     }
 
-    async createDepartment(){
+    getSelectAllQuery(){
+        return `select * from department`
+    }
 
+    getCreateQuery(department){
+        return `
+        insert into departments
+        (name)
+        values
+        (${department.name})
+        `   
+    }
+
+    getUpdateQuery(department){
+        return `
+        update departments 
+        set 
+        name = ${department.name},
+        where id = ${department.id}`
+    }
+
+    getDeleteQuery(department){
+        return `
+        delete from departments 
+        where id = ${department.id}`
+    }
+
+    async createDepartment(department){
+        return await this.db.query(this.getCreateQuery(department))
+        .then((result) => {
+            {}
+        })
+        .catch((error) => {throw error})
+    }
+
+    createDepartmentObject(departmentData){
+        const {id,name} = departmentData
+        return new Department(id,name);
     }
 
     async getDepartments(){
-
+        //query the db and return an array of department objects        
+        return await this.db.query(this.getSelectAllQuery())
+        .then(result => result.map(e => this.createDepartmentObject(e))) //get the db results and make department instances from them
+        .catch((error) => {throw error})
     }
 
-    async updateDepartment(){
-
+    async updateDepartment(department){
+        return await this.db.query(this.getUpdateQuery(department))
+        .then((result) => {})
+        .catch((error) => {throw error})
     }
 
     async deleteDepartment(){
-
+        return await this.db.query(this.getDeleteQuery(department))
+        .then((result) => {})
+        .catch((error) => {throw error})
     }
 
 }
--- Develop/db/roleRepo.js
+++ Develop/db/roleRepo.js
@@ -3,29 +3,68 @@ var Role = require("../model/role")
 
 class roleRepositry {
 
-    constructor(mySQLConnection) {
-        this.connection = mySQLConnection;
-
-        this.createQuery = ``
-        this.selectAllQuery = ``
-        this.updateQuery = ``
-        this.deleteQuery = ``
+    constructor(mySQLDB) {
+        this.db = mySQLDB
+    }
+    getSelectAllQuery(){
+        return "select * from `role`"
     }
 
-    async createRole(){
+    getCreateQuery(role){
+        return "insert into `role`" + 
+        `
+        (first_name,last_name,role_id,manager_id)
+        values
+        (${role.firstName},${role.lastName},${role.role.id},${role.manager.id})
+        `   
+    }
 
+    getUpdateQuery(role){
+        return "update `role`" +
+        `
+        set 
+        title = ${role.title},
+        salary = ${role.salary},
+        deparment_id = ${role.deparment.id},
+        where id = ${role.id}`
+    }
+
+    getDeleteQuery(role){
+        return "delete from role" +
+        `
+        where id = ${role.id}`
+    }
+
+    async createRole(role){
+        return await this.db.query(this.getCreateQuery(role))
+        .then((result) => {
+            {}
+        })
+        .catch((error) => {throw error})
+    }
+
+    createRoleObject(roleData){
+        const {id,title,salary,deparmtent_id} = roleData
+        return new Role(id,title,salary,deparmtent_id);
     }
 
     async getRoles(){
-
+        //query the db and return an array of role objects        
+        return await this.db.query(this.getSelectAllQuery())
+        .then(result => result.map(e => this.createRoleObject(e))) //get the db results and make role instances from them
+        .catch((error) => {throw error})
     }
 
-    async updateRole(){
-
+    async updateRole(role){
+        return await this.db.query(this.getUpdateQuery(role))
+        .then((result) => {})
+        .catch((error) => {throw error})
     }
 
     async deleteRole(){
-
+        return await this.db.query(this.getDeleteQuery(role))
+        .then((result) => {})
+        .catch((error) => {throw error})
     }
 
 }
